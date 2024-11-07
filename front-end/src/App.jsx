e1837512bee291838e2f70831bdb34a18d5a8019
NitroRyder <benjamin.valenzuela.p@usach.cl>
2024-11-07 10:41:51 -0300

    hash

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import ClientHome from './components/ClientHome';
 import RegisterUsuario from './components/RegisterUsuario';
